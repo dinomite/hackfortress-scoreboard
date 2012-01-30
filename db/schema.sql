ecc251cc9d96795a298c220b7c562ee07f63d5ca
Drew Stephens <drew@dinomite.net>
2012-01-30 16:28:54 -0500

    Rename score to hackScore, add tf2Score, and update field widths

--- db/schema.sql
+++ db/schema.sql
@@ -32,7 +32,7 @@ CREATE TABLE `round_has_puzzle` (
 
 DROP TABLE IF EXISTS `team`;
 CREATE TABLE `team` (
-    `teamID` tinyint(1) unsigned NOT NULL auto_increment,
+    `teamID` tinyint(2) unsigned NOT NULL auto_increment,
     `name` varchar(64) NOT NULL default '',
     PRIMARY KEY (`teamID`),
     KEY (`teamID`, `name`)
@@ -40,24 +40,39 @@ CREATE TABLE `team` (
 
 DROP TABLE IF EXISTS `round`;
 CREATE TABLE `round` (
-    `roundID` tinyint(1) unsigned NOT NULL auto_increment,
+    `roundID` tinyint(2) unsigned NOT NULL auto_increment,
     `roundName` enum('First', 'Semi-final', 'Final') NOT NULL default 'First',
-    `red_team` tinyint(1) unsigned NOT NULL,
-    `blue_team` tinyint(1) unsigned NOT NULL,
+    `red_team` tinyint(2) unsigned NOT NULL,
+    `blue_team` tinyint(2) unsigned NOT NULL,
     PRIMARY KEY (`roundID`),
     FOREIGN KEY (`red_team`) REFERENCES `team` (`teamID`),
     FOREIGN KEY (`blue_team`) REFERENCES `team` (`teamID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `score`;
-CREATE TABLE `score` (
-    `scoreID` smallint(1) unsigned NOT NULL auto_increment,
-    `teamID` tinyint(1) unsigned NOT NULL,
-    `roundID` tinyint(1) unsigned NOT NULL,
-    `puzzleID` tinyint(1) unsigned NOT NULL,
-    PRIMARY KEY (`scoreID`),
+DROP TABLE IF EXISTS `hackScore`;
+CREATE TABLE `hackScore` (
+    `hackScoreID` smallint(4) unsigned NOT NULL auto_increment,
+    `roundID` tinyint(2) unsigned NOT NULL,
+    `teamID` tinyint(2) unsigned NOT NULL,
+    `puzzleID` tinyint(3) unsigned NOT NULL,
+    `created` timestamp NOT NULL default CURRENT_TIMESTAMP,
+    PRIMARY KEY (`hackScoreID`),
+    KEY (`roundID`, `teamID`),
     UNIQUE KEY (`teamID`, `puzzleID`),
     FOREIGN KEY (`teamID`) REFERENCES `team` (`teamID`),
     FOREIGN KEY (`roundID`) REFERENCES `round` (`roundID`),
     FOREIGN KEY (`puzzleID`) REFERENCES `puzzle` (`puzzleID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `tf2Score`;
+CREATE TABLE `tf2Score` (
+    `tf2ScoreID` mediumint(4) unsigned NOT NULL auto_increment,
+    `value` tinyint(2) unsigned NOTN NULL,
+    `roundID` tinyint(2) unsigned NOT NULL,
+    `teamID` tinyint(2) unsigned NOT NULL,
+    `created` timestamp NOT NULL default CURRENT_TIMESTAMP,
+    PRIMARY KEY (`tf2ScoreID`),
+    KEY (`roundID`, `teamID`),
+    FOREIGN KEY (`teamID`) REFERENCES `team` (`teamID`),
+    FOREIGN KEY (`roundID`) REFERENCES `round` (`roundID`),
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
